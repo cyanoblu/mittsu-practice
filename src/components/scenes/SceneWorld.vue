48374fde2eaa7587d62972ecc0e2aeb65b3a59b8
cyanoblu <barbosa.raphael@pm.me>
2023-12-29 10:19:36 -0300

    working on the World component from study material

---
+++ src/components/scenes/SceneWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>ok</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue'
+import { World } from '../world/World'
+
+export default defineComponent({
+  setup() {
+    onMounted(() => {
+      const container = document.querySelector('#scene-container')
+      const world = new World(container)
+    })
+
+    World.render()
+    return {}
+  }
+})
+</script>
